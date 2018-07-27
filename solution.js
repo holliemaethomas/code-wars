d6fb9262afaa681c150e79533c84c0782e0da3bc
holliemaethomas <holliemaethomas85@gmail.com>
2018-07-26 19:58:33 -0700

    challenge two

--- solution.js
+++ solution.js
@@ -1,6 +1,4 @@
-function abbrevName(name) {
-  name = name.toUpperCase();
-  const fullName = name.split(' ');
-  const firstInitial = fullName[0][0];
-  const secondInitial = fullName[1][0];
-  return firstInitial + '.' + secondInitial 
+function fixTheMeerkat(arr) {
+  return arr.reverse();
+
+}

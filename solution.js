e27451e096e8a4302607a21e61acd61af731d899
holliemaethomas <holliemaethomas85@gmail.com>
2018-07-31 20:06:11 -0700

    solved

--- solution.js
+++ solution.js
@@ -1,36 +1,4 @@
-function checkExam(array1, array2) {
-  var score = 0;
-
-  for (var i = 0; i < array1.length; i++) {
-
-    if (array1[i] === array2[i]) {
-
-      score += 4;
-
-    }
-
-    else if (array2[i] === "") {
-
-      score += 0;
-
-    }
-
-
-    else {
-
-      score -= 1;
-
-    }
-
-
-  }
-
-  if (score < 0) {
-
-    score = 0;
-
-  }
-
-  return score;
-
+function insertDash(num) {
+  return (num + '').replace
+    (/([13579])(?=[13579])/g, "$1-");
 }

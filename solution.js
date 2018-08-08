7ece9777b00038a81db39e919d585ec712ccb040
holliemaethomas <holliemaethomas85@gmail.com>
2018-08-07 20:33:23 -0700

    solved

--- solution.js
+++ solution.js
@@ -1,4 +1,20 @@
-function insertDash(num) {
-  return (num + '').replace
-    (/([13579])(?=[13579])/g, "$1-");
+function isNice(arr) {
+  let finalArray = [];
+  for (i = 0; i < arr.length; i++) {
+    if (arr.includes(arr[i] + 1) || arr.includes(arr[i] - 1)) {
+      finalArray.push('true')
+    } else {
+      finalArray.push('false')
+    }
+  }
+  console.log(finalArray);
+  if (!arr.length) {
+    return false;
+  }
+  if (finalArray.includes('false')) {
+    return false;
+  } else {
+    return true;
+  }
+
 }

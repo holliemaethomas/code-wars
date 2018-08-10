a92853486cde330b7570c13f780206a20b8e02ae
holliemaethomas <holliemaethomas85@gmail.com>
2018-08-09 19:49:04 -0700

    code wars 6

--- solution.js
+++ solution.js
@@ -1,20 +1,4 @@
-function isNice(arr) {
-  let finalArray = [];
-  for (i = 0; i < arr.length; i++) {
-    if (arr.includes(arr[i] + 1) || arr.includes(arr[i] - 1)) {
-      finalArray.push('true')
-    } else {
-      finalArray.push('false')
-    }
-  }
-  console.log(finalArray);
-  if (!arr.length) {
-    return false;
-  }
-  if (finalArray.includes('false')) {
-    return false;
-  } else {
-    return true;
-  }
+function disemvowel(str) {
+  return str.replace(/[aeiou]/ig, '')
 
 }

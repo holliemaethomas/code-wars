2954dd15aa252780cdb96cacab3cbfc24387d2e0
holliemaethomas <holliemaethomas85@gmail.com>
2018-08-16 09:51:57 -0700

    finished

--- solution.js
+++ solution.js
@@ -1,4 +1,8 @@
-function disemvowel(str) {
-  return str.replace(/[aeiou]/ig, '')
-
-}
+var gimme = (inputArray) => {
+  const sortArray = inputArray.slice().sort((a, b) => a - b);
+  for (i in sortArray) {
+    if (inputArray[i] === sortArray[1]) {
+      return inputArray.indexOf(sortArray[1]);
+    }
+  }
+};

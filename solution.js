6ca99375c9806ad0036eddb7340902ce571709ed
holliemaethomas <holliemaethomas85@gmail.com>
2018-08-16 14:06:32 -0700

    solved

--- solution.js
+++ solution.js
@@ -1,8 +1,3 @@
-var gimme = (inputArray) => {
-  const sortArray = inputArray.slice().sort((a, b) => a - b);
-  for (i in sortArray) {
-    if (inputArray[i] === sortArray[1]) {
-      return inputArray.indexOf(sortArray[1]);
-    }
-  }
-};
+function solution(str) {
+  return str.split("").reverse().join("");
+}
